75ccfe36c0f774239c2a1d15209f13f7a18c0b87
Arli Jose Giron <ajgirondev@icloud.com>
2021-02-02 15:40:21 -0600

    Fin seccion 3

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require ('express');
+//app de express
+const app = express();
+
+const path = require('path');
+require('dotenv').config(); 
+
+
+//node server
+const server = require('http').createServer(app);
+module.exports.io = require('socket.io')(server);
+require('./sockets/socket')
+
+
+
+
+
+//path publico
+ const publicPath = path.resolve(__dirname, 'public');
+
+ app.use( express.static(publicPath));
+
+
+server.listen( process.env.PORT, (err)=>{
+
+    if (err) throw new Error (err);
+
+    console.log('Servidor corriendo en puerto!!!', process.env.PORT);
+  
+
+}); 
